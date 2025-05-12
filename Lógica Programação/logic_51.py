a3419ebf6f0709ae51dc37936ca65905597b1c64
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-05-12 15:02:08 -0300

    addicing logics
    adicionando as lógicas 49, 50 e 51.

---
+++ Lógica Programação/logic_51.py
@@ -0,0 +1,8 @@
+# Generator expression, Itarables e Itaratores em Python
+
+iterable = ['Eu', 'Tenho', '__iter__']
+iterator = iter(iterable) # tem __iter__ e __next__
+
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
