233bf3f397fad0f0a7599bf60fc8715d6d0c3998
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-05-13 09:42:45 -0300

    addicing logics
    adicionando novos códigos na lógica 51, e adicionando a lógica 52.

--- Lógica Programação/logic_51.py
+++ Lógica Programação/logic_51.py
@@ -1,8 +1,19 @@
-# Generator expression, Itarables e Itaratores em Python
+# Generator expression, Itarables e Itaratores em Python.
+# Generator são funções produzem valores sob demanda.
 
 iterable = ['Eu', 'Tenho', '__iter__']
 iterator = iter(iterable) # tem __iter__ e __next__
 
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+
+import sys
+
+lista = [n for n in range(1000000)]
+generator = (n for n in range(1000000))
+
+print(sys.getsizeof(lista))
+print(sys.getsizeof(generator))
+
+print(generator)
