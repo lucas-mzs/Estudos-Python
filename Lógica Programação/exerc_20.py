b049297394519e7eb42892cc6e3672749bfed191
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-05-15 09:00:10 -0300

    adding exercise
    adicionando o exercício 20.

--- Lógica Programação/exerc_20.py
+++ Lógica Programação/exerc_20.py
@@ -1,14 +1,8 @@
 # Exercício:
+import copy
 
 # Aumente os preços dos produtos em 10%.
-# Gere novos_produtos por depy copy (cópia profunda).
-
-# Ordene os produtos por nome decrescente (do maior para o menor).
-# Gere produtos_ordenados_por_nome por depy copy (cópia profunda).
-
-# Ordene os produtos por preço crescente (do menor para o maior).
-# Gere produtos_ordenados_por_preco por depy copy(cópia profunda).
-
+# Gere novos_produtos por deep copy (cópia profunda).
 produtos = [
     {'nome': 'Produto 5', 'preco': 10.00},
     {'nome': 'Produto 1', 'preco': 22.32},
@@ -17,8 +11,34 @@ produtos = [
     {'nome': 'Produto 4', 'preco': 69.90},
 ]
 
-produtos['preco'] = float(produtos['preco'])
+novos_produtos = [
+    {**p, 'preco': round(p['preco'] * 1.1, 2)}
+    for p in copy.deepcopy(produtos)
+]
 
-preco = produtos['preco']
-aumento = preco * 0.10
-print(aumento)
+
+# Ordene os produtos por nome decrescente (do maior para o menor).
+# Gere produtos_ordenados_por_nome por deep copy (cópia profunda).
+produtos_ordenados_por_nome = sorted(
+    copy.deepcopy(produtos),
+    key=lambda p: p['nome'],
+    reverse=True
+)
+
+# Ordene os produtos por preço crescente (do menor para o maior).
+# Gere produtos_ordenados_por_preco por deep copy(cópia profunda).
+
+produtos_ordenados_por_preco = sorted(
+    copy.deepcopy(produtos),
+    key=lambda p: p['preco']
+)
+
+
+# FINAL:
+print(*produtos, sep='\n')
+print()
+print(*novos_produtos, sep='\n')
+print()
+print(*produtos_ordenados_por_nome, sep='\n')
+print()
+print(*produtos_ordenados_por_preco, sep='\n')
