b00c4d81e45d57e74296585d1c22ba13a70f7c7c
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-05-15 09:16:21 -0300

    adding exercise
    adicionando o exercício 21.

---
+++ Lógica Programação/exerc_21.py
@@ -0,0 +1,20 @@
+# Exercício:
+
+# Adiando execução de funções>
+
+def soma(x, y):
+    return x + y
+
+def multiplaca(x, y):
+    return x * y
+
+def criar_funcao(funcao, x):
+    def interna(y):
+        return funcao(x, y)
+    return interna
+
+samo_com_cinco = criar_funcao(soma, 5)
+multiplaca_por_dez = criar_funcao(multiplaca, 10)
+
+print(samo_com_cinco(10))
+print(multiplaca_por_dez(10))
