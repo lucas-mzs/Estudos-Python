3b6c501cb531a6963b36123d38d161448e9d9952
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-06-05 19:12:44 -0300

    adding codes in logic.
    adicionando os códigos do exercício no arquivo.

--- Lógica Programação/exerc_24.py
+++ Lógica Programação/exerc_24.py
@@ -2,41 +2,77 @@
 
 # Lista de tarefas com desfazer e refazer.
 
+import os
+
 def listar(tarefas):
     print()
     if not tarefas:
-        print('Nenhuma tarefa para listar')
+        print('Nenhuma tarefa para listar.')
         return
     
     print('Tarefas:')
     for tarefa in tarefas:
         print(f'\t{tarefa}')
-    
-def desfazer(tarefas, tarefas_desfazer):
+
+def desfazer(tarefas, tarefas_refazer):
     print()
     if not tarefas:
-        print('Nenhuma tarefa para desfazer')
+        print('Nenhuma tarefa para desfazer.')
         return
     
     tarefa = tarefas.pop()
-    tarefas_desfazer.append(tarefa)
+    print(f'{tarefa}: removida da lista de tarefas.')
+    tarefas_refazer.append(tarefa)
+
+def refazer(tarefas, tarefas_refazer):
+    print()
+    if not tarefas_refazer:
+        print('Nenhuma tarefa para refazer.')
+        return
+    
+    tarefa = tarefas_refazer.pop()
+    print(f'{tarefa}: adicionada na lista de tarefas.')
+    tarefas.append(tarefa)
+
+def adicionar(tarefa, tarefas, tarefas_refazer):
+    print()
+    tarefa = tarefa.strip()
+    if not tarefa:
+        print('Você não digitou nenhuma tarefa.')
+        return
+    
+    print(f'{tarefa}: adicionada na lista de tarefas.')
+    tarefas.append(tarefa)
+    tarefas_refazer.clear()
+
 
 tarefas = []
 tarefas_refazer = []
 
+
 while True:
-    print('Comandos: listar, desfazer e refazer.')
+    print('Comandos: listar, desfazer e refazer ou digite uma nova tarefa.')
     tarefa = input('Digite uma tarefa ou comando: ')
 
     if tarefa == 'listar':
         listar(tarefas)
+        print()
         continue
     elif tarefa == 'desfazer':
-        print(2)
+        desfazer(tarefas, tarefas_refazer)
+        listar(tarefas)
+        print()
         continue
     elif tarefa == 'refazer':
-        print(3)
+        refazer(tarefas, tarefas_refazer)
+        listar(tarefas)
+        print()
+        continue
+    elif tarefa == 'limpar':
+        os.system('cls')
         continue
     else:
-        tarefas.append(tarefa)
+        adicionar(tarefa, tarefas, tarefas_refazer)
+        listar(tarefas)
+        print()
         continue
