95e8322f08db755ed18f046f362e379256bb2363
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-06-11 20:09:29 -0300

    improving the exercises codes.

--- Lógica Programação/exerc_07.py
+++ Lógica Programação/exerc_07.py
@@ -1,17 +1,16 @@
 # Exercício
 
-# Faça um prgrama que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou menos escreva "Seu nome é curto";
-# se tiver entre 5 e 6 letras, escreva "Seu nome é nomral"; maior que 6 escreva "Seu nome é muito grande".
+# Faça um prgrama que peça o primeiro nome do usuário:
+ 
+# Se o nome tiver 4 letras ou menos escreva "Seu nome é curto";
+# Se tiver entre 5 e 6 letras, escreva "Seu nome é normal";
+# Maior que 6 escreva "Seu nome é muito grande".
 
-nome = input('Digite seu nome: ')
-tamanho_nome = len(nome)
+nome = len(input('Digite seu nome: '))
 
-if tamanho_nome > 2:
-    if tamanho_nome <= 4:
-        print('Seu nome é muito curto!')
-    elif tamanho_nome >= 5 and tamanho_nome <= 6:
-        print('Seu nome é normal!')
-    else:
-        print('Seu nome é muito grande!')
+if nome <= 4:
+    print('Seu nome é muito curto.')
+elif nome >= 5 and nome <= 6:
+    print('Seu nome é normal.')
 else:
-    print('Digite mais de uma letra.')
+    print('Seu nome é grande.')
--- Lógica Programação/exerc_09.py
+++ Lógica Programação/exerc_09.py
@@ -3,27 +3,24 @@
 # Calculadora com while
 
 while True:
-    numero_1 = input('Digite um número: ')
-    numero_2 = input('Digite outro número: ')
+    numero_1 = float(input('Digite um número: '))
+    numero_2 = float(input('Digite outro número: '))
     operador = input('Digite um operador ( +-/* ): ')
 
     numeros_validos = None
 
-    num_1_float = 0
-    num_2_float = 0
-
     try:
-        num_1_float = float(numero_1)
-        num_2_float = float(numero_2)
+        numero_1
+        numero_2
         numeros_validos = True
     except:
-         numeros_validos = None
+        numeros_validos = None
 
     if numeros_validos is None:
-        print('Um ou ambos os números digitados são inválidos.')
+        print('Um ou ambos os números não são válidos.')
         continue
 
-    operadores_permitidos = '+-/*'
+    operadores_permitidos = ' + - / * '
 
     if operador not in operadores_permitidos:
         print('Operador inválido.')
@@ -34,16 +31,51 @@ while True:
         continue
 
     print('Realizando sua conta. Confira o resultado abaixo:')
+
     if operador == '+':
-        print(f'{num_1_float} + {num_2_float} = ', num_1_float + num_2_float)
+        print(f'{numero_1} + {numero_2}: ', numero_1 + numero_2)
     elif operador == '-':
-        print(f'{num_1_float} - {num_2_float} = ', num_1_float - num_2_float)
+        print(f'{numero_1} - {numero_2}: ', numero_1 - numero_2)
     elif operador == '/':
-        print(f'{num_1_float} / {num_2_float} = ', num_1_float / num_2_float)
+        print(f'{numero_1} / {numero_2}: ', numero_1 / numero_2)
     elif operador == '*':
-        print(f'{num_1_float} * {num_2_float} = ', num_1_float * num_2_float)
+        print(f'{numero_1} * {numero_2}: ', numero_1 * numero_2)
 
     sair = input('Quer sair? [s]im: ').lower().startswith('s')
 
     if sair is True:
         break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
