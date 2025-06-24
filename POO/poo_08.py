43e4e623b9cf913f6f4c59cdc0def5d8106aae9f
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-06-24 13:13:16 -0300

    adding the new poo

---
+++ POO/poo_08.py
@@ -0,0 +1,26 @@
+# __dict__ e vars para atributos de instância
+
+class Pessoa:
+    ano_atual = 2022
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+        def ano_nascimento(self):
+            return Pessoa.ano_atual - self.idade
+        
+dados = {'nome': 'João', 'idade': 35}
+p1 = Pessoa(**dados)
+# p1.nome = 'EITA'
+# print(p1.idade)
+# p1.__dict__['outra'] = 'coisa'
+# p1.__dict__['nome'] = 'EITA'
+# del p1.__dict__['nome']
+# print(p1.__dict__)
+# print(vars(p1))
+# print(p1.outra)
+# print(p1.idade)
+# print(p1.nome)
+# print(vars(p1))
+print(p1.nome)
