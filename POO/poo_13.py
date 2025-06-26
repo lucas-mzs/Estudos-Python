7e00ade1029765b00305eafbf551d68b34b58066
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-06-26 20:55:32 -0300

    adding a new  poo

---
+++ POO/poo_13.py
@@ -0,0 +1,37 @@
+# @property + @setter - getter e setter no modo Pythônico
+# Geralmente é usada nas seguintes situações:
+# - como getter
+# - para evitar quebrar código cliente
+# - para habilitar setter
+# - para executar ações ao obter um atributo
+# Atributos que começar com um ou dois underlines não devem ser usados fora da classe.
+
+class Caneta:
+    def __init__(self, cor):
+        # private protected
+        self._cor = cor
+        self._cor_tampa = None
+
+    @property
+    def cor(self):
+        print('ESTOU NO GETTER')
+        return self._cor
+    
+    @cor.setter
+    def cor(self, valor):
+        print('ESTOU NO SETTER')
+        self._cor = valor
+
+    @property
+    def cor_tampa(self):
+        return self._cor_tampa
+    
+    @cor_tampa.setter
+    def cor_tampa(self, valor):
+        self._cor_tampa = valor
+
+caneta = Caneta('Azul')
+caneta.cor = 'Rosa'
+caneta.cor_tampa = 'Azul'
+print(caneta.cor)
+print(caneta.cor_tampa)
