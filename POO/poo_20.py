3bf8bb6d14b03dc0ec113c9e204430da495df890
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-07-01 11:23:27 -0300

    adding a new poo

---
+++ POO/poo_20.py
@@ -0,0 +1,24 @@
+# Herança Múltipla
+# Quer dizer que no Python, uma classe pode estender várias outras classes.
+# Herança Simples:
+# Animal -> Mamifero -> Humano -> Pessoa -> Cliente
+# Herança múltipla e mixins
+# Log -> FileLog
+# Animal -> Mamifero -> Humano -> Pessoa -> Cliente
+# Cliente(Pessoa, FileLog)
+
+# A, B, C, D
+# D(B, C) - C(A) - B(A) - A
+#           A
+#         /   \
+#        B     C
+#         \   /
+#           D
+
+# Python 3 usa C3 superclass linearzation para gerar o mro.
+# https://en.wikipedia.org/wiki/C3_linearzation
+
+# Para saber a ordem de chamada dos método:
+# Use o método de classe Classe.mro()
+# Ou o atributo __mro__ 
+# (Dunder - Double Underscore)
