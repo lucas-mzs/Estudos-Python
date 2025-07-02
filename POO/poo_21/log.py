ce5ec88b5405919963f29de13a7c1ba0155e8cff
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-07-01 23:40:20 -0300

    modying codes

--- POO/poo_21/log.py
+++ POO/poo_21/log.py
@@ -3,15 +3,18 @@
 from pathlib import Path
 
 LOG_FILE = Path(__file__).parent / 'log.txt'
+
+
 class Log:
     def _log(self, msg):
         raise NotImplementedError('Implemente o método log')
-    
+
     def log_error(self, msg):
         self._log(f'Error: {msg}')
 
     def log_success(self, msg):
         self._log(f'Success: {msg}')
+
 
 class LogFileMixin(Log):
     def _log(self, msg):
@@ -21,12 +24,14 @@ class LogFileMixin(Log):
             arquivo.write(msg_formatada)
             arquivo.write('\n')
 
-class LogPrintMixin(Log):
+
+class LogFileMixin(Log):
     def _log(self, msg):
         print(f'{msg} ({self.__class__.__name__})')
 
+
 if __name__ == '__main__':
-    lp = LogPrintMixin()
+    lp = LogFileMixin()
     lp.log_error('qualquer coisa')
     lp.log_success('Que legal')
 
--- POO/poo_21/main.py
+++ POO/poo_21/main.py
@@ -1,9 +1,17 @@
-from log import LogPrintMixin, LogFileMixin
+# from log import LogPrintMixin, LogFileMixin
 
-lp = LogPrintMixin()
-lp.log_error('qualquer coisa')
-lp.log_success('Que legal')
+# lp = LogPrintMixin()
+# lp.log_error('qualquer coisa')
+# lp.log_success('Que legal')
 
-lf = LogFileMixin()
-lf.log_error('qualquer coisa')
-lf.log_success('Que legal')
+# lf = LogFileMixin()
+# lf.log_error('qualquer coisa')
+# lf.log_success('Que legal')
+
+from elotronico import SmartPhone
+
+galaxy_s = SmartPhone('Galaxy S')
+iphone = SmartPhone('iPhone')
+
+galaxy_s.ligar()
+iphone.desligar()
