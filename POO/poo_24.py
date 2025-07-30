89633a95d11ebcebb7f91835c70f404a4bd60f4d
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-07-30 11:36:08 -0300

    adding the contents of the poo 24

--- POO/poo_24.py
+++ POO/poo_24.py
@@ -6,3 +6,37 @@
 # SO"L"ID
 # Princípios da substituição de liskov
 # Objetos de uma superclass devem ser substituíveis por objetos de uma subclasse sem quebrar a aplicação.
+# Sobrecarga de métodos (overload) 🐍 = ❌
+# Sobreposição de métodos (override) 🐍 = ✅
+
+from abc import ABC, abstractmethod
+class Notificacao(ABC):
+    def __init__(self, mensagem):
+        self.mensagem = mensagem
+    
+    @abstractmethod
+    def enviar(self) -> bool: ...
+
+class NotificacaoEmail(Notificacao):
+    def enviar(self) -> bool:
+        print(f'E-mail: Enviando: {self.mensagem}')
+        return True
+    
+class NotificacaoSMS(Notificacao):
+    def enviar(self) -> bool:
+        print(f'SMS: Enviando: {self.mensagem}')
+        return False
+
+def notificar(notificacao: Notificacao):
+    notificacao_enviada = notificacao.enviar()
+
+    if notificacao_enviada:
+        print('Notificação enviada.')
+    else:
+        print('Notificação NÃO enviada.')
+
+notificacao_email = NotificacaoEmail('testando e-mail.')
+notificar(notificacao_email)
+
+notificacao_sms = NotificacaoSMS('testando SMS.')
+notificar(notificacao_sms)
