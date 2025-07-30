cfccef9dfc53337fab22e87fc571dc46ea4b8e9d
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-07-29 22:52:42 -0300

    adding the beginning of a poo

---
+++ POO/poo_24.py
@@ -0,0 +1,8 @@
+# Polimorfismo em Python Orientado a Objetos.
+# Polimorfismo é o princípio que permite que classes derivadas de uma mesma superclasse tenham métodoa 
+# iguais (com mesma assinatura) mas comportamentos diferentes.
+# Assinatura do método = Mesmo nome e quantidade de parâmetros (retorno não faz parte da assinatura).
+# Opnião + princípios que contam:
+# SO"L"ID
+# Princípios da substituição de liskov
+# Objetos de uma superclass devem ser substituíveis por objetos de uma subclasse sem quebrar a aplicação.
