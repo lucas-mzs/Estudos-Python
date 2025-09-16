2f742bc6faa2fec10f587161b8ba678db4ecbcf1
Lucas Felipe <lucas.felipe.lfmf@gmail.com>
2025-09-16 14:34:08 -0300

    adding the new codes to an existing file

--- Módulos Python/datas.py
+++ Módulos Python/datas.py
@@ -10,14 +10,25 @@
 # pip install pytz types-pytz
 
 from datetime import datetime
+from pytz import timezone
+
+data_lugares = datetime.now(timezone('Asia/Tokyo'))
+# print(data_lugares)
 
 data_str_data1 = '2025-09-15 14:40:25'
 data_str_fmt1 = '%Y-%m-%d %H:%M:%S'
 data_str_data2 = '15-09-2025'
 data_str_fmt2 = '%d-%m-%Y'
 
-# data = datetime(2025, 9, 15, 14, 33)
+data = datetime(2025, 9, 15, 14, 33)
+
 data1 = datetime.strptime(data_str_data1, data_str_fmt1)
 data2 = datetime.strptime(data_str_data2, data_str_fmt2)
-print(data1)
-print(data2)
+# print(data1)
+# print(data2)
+
+data_agora = datetime.now()
+# print(data_agora)
+
+criar_data = datetime.now()
+# print(criar_data.timestamp())
